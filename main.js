3c2982194196dff69106191bf9963a9d0bbf5e7e
Marius Kristiansen <mariussk@getacademy.no>
2019-04-08 10:52:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var sticky;
 var navbar;
+var blogData;
 function scrollSticky() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
@@ -13,8 +14,21 @@ function init() {
 
     navbar = document.getElementById('navbar');
     sticky = navbar.offsetTop;
+    loadJSON();
+}
+
+function loadJSON() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        blogData = JSON.parse(this.responseText);
+    }
+};
+xmlhttp.open("GET", "blog.json", true);
+xmlhttp.send();
 }
 
 function parseJSON() {
     const filename = "blog.json";
+    let result = JSON.parse
 }
